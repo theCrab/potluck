ef44a036a713df8529418f7e91634fde8da2087b
Nelson Kelem <nelson@21waves.com>
2013-06-14 02:15:48 +0100

    Add Taxi::Vehicle tests

---
+++ spec/unit/taxi/vehicle_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe Taxi::Vehicle do
+  it "should have a make"
+  it "has a model"
+  it "has a Date of registration"
+  it "has Seats"
+  it "has valid MOT"
+  it "has valid Insurance"
+  it "has valid Road-tax"
+  it "has valid Taxi Lincense Badge"
+end
